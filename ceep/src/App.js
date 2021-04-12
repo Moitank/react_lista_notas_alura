3ab8baa189c214b6a37d0af10b76c8183fae45a1
Marcelo Moita <marcelo.moita@al.resilia.com.br>
2021-04-12 11:01:28 -0300

    aplicacao finalizada

---
+++ ceep/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import ListaDeNotas from "./Components/ListaDeNotas";
+import FormularioCadastro from "./Components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+
+  constructor() {
+    super();
+    
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {titulo, texto};
+    const novoArrrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrrayNotas
+    }
+    
+    this.setState(novoEstado)
+  
+  }
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
+}
+
+export default App;
